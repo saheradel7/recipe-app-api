d13ddcd9d44e25f3af0b37e0914661e0d25ad04f
saheradel7 <s.adelhlmy@gmail.com>
2025-02-13 02:49:42 +0200

    fix linting issues 4

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,7 +7,6 @@ from rest_framework import status
 CREATE_USER_URL = reverse("user:create")
 TOKEN_URL = reverse("user:token")
 
-#helper function
 def create_test_user(**params):
     return get_user_model().objects.create_user(**params)
 
